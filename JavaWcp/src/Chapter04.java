11738306771e33500da2cb1aa4b2bb1d20023afe
000ATSU <gaomuliren706@gmail.com>
2022-09-26 17:37:20 +0900

    [add]New Project

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int lukey_number = 24;
+		String str1;
+		str1 = "アットマーク";
+		System.out.println(str1);
+		str1 = "find a way";
+		System.out.println(str1);
+		System.out.println(lukey_number);
+	}
+}
---
+++ JavaWcp/src/Chapter5.java
@@ -0,0 +1,27 @@
+
+public class Chapter5 {
+	public static void main(String[] args) {
+		long long1 = 1010100000L;
+		double double1 = 10.02;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		Float float1 = 0.111f;
+		System.out.println(float1);
+		Integer integer1 = 555;
+		System.out.println(integer1);
+		Character char1 = 0x3042;
+		System.out.println(char1);
+		int[] array = new int[5];
+		array[0] = 1;
+		array[1] = 2;
+		array[2] = 3;
+		array[3] = 4;
+		array[4] = 5;
+		int int1 = array[2];
+		System.out.println(int1);
+		int[] array1 = { 0, 1, 3, 4, 5 };
+		System.out.println(array1[3]);
+	}
+}
